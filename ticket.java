2fccc45751a33af23afc81c9eb34953dff6278aa
pratyasha-dakshinray <pratyashaprakash@gmail.com>
2026-07-13 17:37:19 +0530

    Ticket id is created

---
+++ ticket.java
@@ -0,0 +1,10 @@
+java.util.Date;
+java.util.List;
+public class ticket {
+    private String ticketid;
+    private String userid;
+    private String source;
+    private String destination;
+    private Date dateofTravel;
+    private train train;
+}
