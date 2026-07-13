a282a65c406824f639b295407d4c2d47220f3438
pratyasha-dakshinray <pratyashaprakash@gmail.com>
2026-07-13 17:37:34 +0530

    Train id is created

---
+++ train.java
@@ -0,0 +1,12 @@
+java.util.List;
+java.util.Map;
+
+public class train {
+    private String trainid;
+    private String trainno;
+    private List<List<Integer>> seats;
+    private Map<String, Time> stationTime;
+
+
+
+}
